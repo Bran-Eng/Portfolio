b6e458c573ae7c9c83050fce3253aafe7b11da01
Brandon Soria <brandon.se.25@gmail.com>
2024-04-09 07:39:56 -0600

    updated code

--- 0-portfolio-web/portfolio/components/main/StarBackground.tsx
+++ 0-portfolio-web/portfolio/components/main/StarBackground.tsx
@@ -6,9 +6,43 @@ import { Points, PointMaterial, Preload } from '@react-three/drei';
 // @ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm';
 
-const StarBackground = () => {
-  return <div>StarBackground</div>;
-  random;
+const StarBackground = (props: any) => {
+  const ref: any = useRef();
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 33;
+    ref.current.rotation.y -= delta / 38;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled
+        {...props}>
+        <PointMaterial
+          transparent
+          color='$fff'
+          size={0.002}
+          sizeAttenuation={true}
+          dethWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
-export default StarBackground;
+const StarsCanvas = () => (
+  <div className='w-full h-auto fixed inset-0 z-[20]'>
+    <Canvas camera={{ position: [0, 0, 1] }}>
+      <Suspense fallback={null}>
+        <StarBackground />
+      </Suspense>
+    </Canvas>
+  </div>
+);
+
+export default StarsCanvas;
