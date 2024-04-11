a9da9a34e5370788ce3367a0c1326e3207de99b7
Brandon Soria <brandon.se.25@gmail.com>
2024-04-11 08:18:20 -0600

    update code

--- 0-portfolio-web/portfolio/components/main/Hero.tsx
+++ 0-portfolio-web/portfolio/components/main/Hero.tsx
@@ -3,19 +3,18 @@ import HeroContent from '../sub/HeroContent';
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
+    <div className='relative flex flex-col h-full w-full '>
       <video
         autoPlay
         muted
         loop
         className='rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'>
         <source
-          src='blackhole_blue_2.webm'
+          // src='blackhole_blue_2.webm'
+          src='ppp.mov'
           type='video/webm'
         />
       </video>
-
-      <div className='flex flex-col absolute top-0 left-0 right-0 bottom-[1340px] z-20 bg-blue-600 bg-opacity-10'></div>
 
       <HeroContent></HeroContent>
     </div>
